d287b082a2dd80dfc9b4d9d51336b92178253568
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2021-11-07 21:44:19 +0200

    Update JenkinsFile.groovy

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,12 +1,19 @@
 node('master') {
+  stage('trigger Terraform) {
+        build wait: false, job: 'terraform-run', parameters: [credentials(name: 'aws-creds', value: 'aws')]
+        }
     stage('checkout code') {
         cleanWs()
         checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/lidorg-dev/nodejs.org.git']]])
     }
     stage('build') {
         sh 'npm install' 
+        sleep 15s
     }
     stage('test') {
+      //  sh "node server.js &"
+    }
+     stage('Terraform Destroy ') {
       //  sh "node server.js &"
     }
     stage('Package') {
