97487a01d7d37579968add530d6cec24534b9f69
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2021-11-07 19:03:13 +0200

    Add files via upload

---
+++ JenkinsFile.groovy
@@ -0,0 +1,15 @@
+node('master') {
+    stage('checkout code') {
+        cleanWs()
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/lidorg-dev/nodejs.org.git']]])
+    }
+    stage('build') {
+        sh 'npm install' 
+    }
+    stage('test') {
+      //  sh "node server.js &"
+    }
+    stage('Package') {
+        sh "tar -czvf package.tar.gz *"
+        archiveArtifacts artifacts: 'package.tar.gz', followSymlinks: false
+    }
